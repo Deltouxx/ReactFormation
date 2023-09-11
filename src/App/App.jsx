107f3d72d7ede54addf9d20b19d4404b9c0b1aee
Alexis Theduke <alexis.leduc.j@gmail.com>
2023-09-11 12:15:07 +0100

    ajout bouton

---
+++ src/App/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Button from './components/Button/Button.jsx';
+
+/**
+ * 
+ * @param {*} props 
+ * @returns 
+ */
+
+const App=(props) => {
+
+
+return <div className='App'>Michel c'est le bresil <hr/> <Button text='Benjamin' /> <hr/>
+<Button text='Janson' /> </div>
+
+};
+export default App;
---
+++ src/App/components/Button/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button=(props)=> {
+    console.log(props)
+  return (
+    <button className='Button'> {props.text}</button>
+  )
+};
+export default Button
