7baf8b118e3317ef6993cc5b22e66221271960e4
Alexis Theduke <alexis.leduc.j@gmail.com>
2023-09-11 11:52:45 +0100

    Michel c est le brezil - DEMAT BREIZH

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+/**
+ * 
+ * @param {*} props 
+ * @returns 
+ */
+
+const App=(props) => {
+
+
+return <div className='App'>Michel c'est le bresil</div>
+};
+export default App;
