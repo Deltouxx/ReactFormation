c3aa018e8218dc03b3748023df02ece7a4dfa177
Alexis Theduke <alexis.leduc.j@gmail.com>
2023-09-12 11:23:10 +0100

    Header et footer

--- src/App/components/ui/Header/Header.jsx
+++ src/App/components/ui/Header/Header.jsx
@@ -4,7 +4,7 @@ import styles from './Header.module.css';
 
 const Header = () => (
   <div className={styles.Header} data-testid="Header">
-    Header Component
+    <span className={styles.meme} >Bienvenue chez THEDUKE COMPANY</span>
   </div>
 );
 
