2aeacc4e7c90d9e1a656bf94817d98e6e6f8158b
Alexis Theduke <alexis.leduc.j@gmail.com>
2023-09-12 11:37:31 +0100

    nav bar - attention au as si jamais meme nom

--- src/App/components/ui/NavBar/NavBar.jsx
+++ src/App/components/ui/NavBar/NavBar.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './NavBar.module.css';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
-const NavBar = () => (
+const NavBar = () => ( 
   <div className={styles.NavBar} data-testid="NavBar">
-    NavBar Component
+          <Navbar bg="primary" data-bs-theme="dark">
+         <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
   </div>
 );
 
