f50aa3f60650e0260992c9d2197a15b0714e6dda
Alexis Theduke <alexis.leduc.j@gmail.com>
2023-09-13 16:59:02 +0100

    svg click nav bar

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App/App.jsx'
 import { Provider } from 'react-redux'
 import {store} from './App/store/store'
+import { BrowserRouter as Router } from 'react-router-dom'
 // import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <Router>
+         <App />
+      </Router>
     </Provider>
   </React.StrictMode>,
 )
