99202bcc23e33953e12d9946fa56d71d7a96c06d
Alexis Theduke <alexis.leduc.j@gmail.com>
2023-09-13 09:07:46 +0100

    reducer

--- src/App/store/current.js
+++ src/App/store/current.js
@@ -3,19 +3,27 @@ import { DummyMeme } from '../interfaces/common';
 
 const initialState = DummyMeme;
 
-const current = createSlice({
-  name: 'current',
-  initialState,
-  reducers: {
-    changeCurrent(state,action){
+// const current = createSlice({
+//   name: 'current',
+//   initialState,
+//   reducers: {
+//     changeCurrent(state,action){
 
-    },
-    clearCurrent(state,action){
+//     },
+//     clearCurrent(state,action){
 
+//     }
+//   }
+// });
+
+// export const {clearCurrent,changeCurrent} = current.actions
+
+// export default current.reducer
+const currentReducer =(state=initialState,action)=>{
+    switch(action.type){
+        case 'current/change':return {...state,...action.payload};
+        case 'current/clear':return {DummyMeme};
+        default : return state;
     }
-  }
-});
-
-export const {clearCurrent,changeCurrent} = current.actions
-
-export default current.reducer
+}
+export default currentReducer
